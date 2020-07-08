40d7d8309352f7abab5e1c722d6252a14c3a2523
Sammuel Reis <sammugr@hotmail.com>
2020-07-07 23:13:04 -0300

    implements repository details layout

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
 	repository: string;
@@ -7,7 +12,55 @@ interface RepositoryParams {
 
 const Repository: React.FC = () => {
 	const { params } = useRouteMatch<RepositoryParams>();
-	return <h1>Repository: {params.repository}</h1>;
+	return (
+		<>
+			<Header>
+				<img src={logoImg} alt="Github Explorer" />
+				<Link to="/">
+					<FiChevronLeft size={16} />
+					Voltar
+				</Link>
+			</Header>
+
+			<RepositoryInfo>
+				<header>
+					<img
+						src="https://avatars0.githubusercontent.com/u/28929274?v=4"
+						alt="rocketseat"
+					/>
+					<div>
+						<strong>rocketseat/unform</strong>
+						<p>Descrição do repositório</p>
+					</div>
+				</header>
+				<ul>
+					<li>
+						<strong>1800</strong>
+						<span>Stars</span>
+					</li>
+					<li>
+						<strong>48</strong>
+						<span>Forks</span>
+					</li>
+					<li>
+						<strong>10</strong>
+						<span>Issues abertas</span>
+					</li>
+				</ul>
+			</RepositoryInfo>
+
+			<Issues>
+				<Link to="rsrs">
+					<div>
+						<strong>tstst</strong>
+						<p>jsjsjs</p>
+					</div>
+
+					<FiChevronRight size={20} />
+				</Link>
+			</Issues>
+		</>
+	);
 };
 
 export default Repository;
